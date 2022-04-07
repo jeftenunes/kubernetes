e5002dfbadaf7ba5a4dda78d20f2c2615c5c75f3
badtux <jeftenunes@hotmail.com>
2022-04-07 19:24:48 -0400

    zero downtime deployments

---
+++ deployments/node-app/v2/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const port = 3001;
+const hostname = "127.0.0.1";
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+
+    res.end("This is V2");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server up and running http://${hostname}:${port}/`);
+})
